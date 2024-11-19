ee021220344a0d376d7e880a626ef0f9de1a36c4
Mariyan Tomov <murcho123@gmail.com>
2024-11-19 09:33:54 +0200

    Initial commit

---
+++ BookApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BooksApp.Models;
+using System.Collections.Generic;
+
+namespace BooksApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookApp/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 2100, ErrorMessage = "Year must be between 1 and 2100")]
+        public int YearPublished { get; set; }
+    }
+}
